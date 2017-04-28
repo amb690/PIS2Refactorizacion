d15fc1fd3eab1fa9bd0ea3fc90a71fd6d18106fb
alberto <albertombln@hotmail.com>
2017-04-28 10:40:19 +0200

    Extracting frequent renter points

--- PIS2Refactoring/src/main/java/org/pis2refactoring/Customer.java
+++ PIS2Refactoring/src/main/java/org/pis2refactoring/Customer.java
@@ -24,15 +24,13 @@ public class Customer {
           Enumeration rentals = _rentals.elements();
           String result = "Rental Record for " + getName() + "\n";
           while (rentals.hasMoreElements()) {
-              double thisAmount = 0;
               Rental each = (Rental) rentals.nextElement();
-              // extracting the amount calculation
-              thisAmount = each.getCharge();
               // add frequent renter points
               frequentRenterPoints ++;
               // add bonus for a two day new release rental
               if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && 
             		  each.getDaysRented() > 1) frequentRenterPoints ++;
+              // extracting the amount calculation
               //show figures for this rental
               result += "\t" + each.getMovie().getTitle()+ "\t" + each.getCharge() + "\n";
               totalAmount += each.getCharge();
