4a9c4be1c9492d66820ebb509de8689e8cdb7cd3
alberto <albertombln@hotmail.com>
2017-04-28 15:04:39 +0200

    Change of customer class

--- PIS2Refactoring/src/main/java/org/pis2refactoring/Customer.java
+++ PIS2Refactoring/src/main/java/org/pis2refactoring/Customer.java
@@ -19,35 +19,27 @@ public class Customer {
 	   };
 	   
 	   public String statement() {
-          double totalAmount = 0;
-          int frequentRenterPoints = 0;
           Enumeration rentals = _rentals.elements();
           String result = "Rental Record for " + getName() + "\n";
           while (rentals.hasMoreElements()) {
               Rental each = (Rental) rentals.nextElement();
-              // add frequent renter points
-              frequentRenterPoints+=each.getFrequentRenterPoints(each.getMovie().getPriceCode());
-              //show figures for this rental
-              result += "\t" + each.getMovie().getTitle()+ "\t" + each.getCharge() + "\n";
-              totalAmount += each.getCharge();
+              result += "\t" + each.getMovie().getTitle()+ "\t" + String.valueOf(getTotalCharge()) + "\n";
           }
-          //add footer lines
-          result +=  "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
-          result += "On this rental you earned <EM>" + String.valueOf(getTotalFrequentRenterPoints()) + "</EM> frequent renter points<P>";
+          result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+          result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
           return result;
       }
 	   
 	  public String htmlStatement(){
-		  Enumeration rentals = _rentals.elements();
+		  Enumeration<Rental> rentals = _rentals.elements();
 		  String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
 		  while (rentals.hasMoreElements()) {
               Rental each = (Rental) rentals.nextElement();
-              //show figures for this rental
-              result += "\t" + each.getMovie().getTitle()+ "\t" + each.getCharge() + "\n";
+              result += "\t" + each.getMovie().getTitle()+ ": " + each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<BR>\n";
           }
           //add footer lines
-          result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-          result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
+          result +=  "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+          result += "On this rental you earned <EM>" + String.valueOf(getTotalFrequentRenterPoints()) + "</EM> frequent renter points<P>";
 		  return result;
 	  }
 	   
