14f657d310cd350d687669379617cb7349860c29
alberto <albertombln@hotmail.com>
2017-04-28 15:24:00 +0200

    Extract differences from Customer class

--- PIS2Refactoring/src/main/java/org/pis2refactoring/HtmlStatement.java
+++ PIS2Refactoring/src/main/java/org/pis2refactoring/HtmlStatement.java
@@ -5,14 +5,26 @@ import java.util.Enumeration;
 public class HtmlStatement {
 	public String value(Customer aCustomer){
 		Enumeration<Rental> rentals = aCustomer.getRentals();
-		  String result = "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
+		  String result = headerString(aCustomer);
 		  while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            result += "\t" + each.getMovie().getTitle()+ ": " + each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+            result += eachRentalString(each);
         }
         //add footer lines
-        result +=  "<P>You owe <EM>" + String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
-        result += "On this rental you earned <EM>" + String.valueOf(aCustomer.getTotalFrequentRenterPoints()) + "</EM> frequent renter points<P>";
+        result += footerString(aCustomer);
 		  return result;
+	}
+	
+	public String headerString(Customer aCustomer){
+		return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
+	}
+	
+	public String eachRentalString(Rental aRental){
+		return "\t" + aRental.getMovie().getTitle()+ ": " + aRental.getMovie().getTitle() + ": " + String.valueOf(aRental.getCharge()) + "<BR>\n";
+	}
+	
+	public String footerString(Customer aCustomer){
+		return "<P>You owe <EM>" + String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n" 
+	+ "On this rental you earned <EM>" + String.valueOf(aCustomer.getTotalFrequentRenterPoints()) + "</EM> frequent renter points<P>";
 	}
 }
