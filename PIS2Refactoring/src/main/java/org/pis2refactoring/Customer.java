967023d07a2aab4c3bc46d2833c604680ca52223
alberto <albertombln@hotmail.com>
2017-04-28 11:00:41 +0200

    htmlStatement implemented

--- PIS2Refactoring/src/main/java/org/pis2refactoring/Customer.java
+++ PIS2Refactoring/src/main/java/org/pis2refactoring/Customer.java
@@ -32,10 +32,24 @@ public class Customer {
               totalAmount += each.getCharge();
           }
           //add footer lines
-          result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-          result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
+          result +=  "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+          result += "On this rental you earned <EM>" + String.valueOf(getTotalFrequentRenterPoints()) + "</EM> frequent renter points<P>";
           return result;
       }
+	   
+	  public String htmlStatement(){
+		  Enumeration rentals = _rentals.elements();
+		  String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+		  while (rentals.hasMoreElements()) {
+              Rental each = (Rental) rentals.nextElement();
+              //show figures for this rental
+              result += "\t" + each.getMovie().getTitle()+ "\t" + each.getCharge() + "\n";
+          }
+          //add footer lines
+          result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+          result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
+		  return result;
+	  }
 	   
 	  private double getTotalCharge(){
 		  double result=0;
