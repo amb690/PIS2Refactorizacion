0fed1cca40a92689dd22c8019246e4488f6d9971
alberto <albertombln@hotmail.com>
2017-04-28 10:54:51 +0200

    Refactoring frequent renter points

--- PIS2Refactoring/src/main/java/org/pis2refactoring/Customer.java
+++ PIS2Refactoring/src/main/java/org/pis2refactoring/Customer.java
@@ -26,14 +26,14 @@ public class Customer {
           while (rentals.hasMoreElements()) {
               Rental each = (Rental) rentals.nextElement();
               // add frequent renter points
-              frequentRenterPoints+=each.frequentRenterPoints();
+              frequentRenterPoints+=each.getFrequentRenterPoints();
               //show figures for this rental
               result += "\t" + each.getMovie().getTitle()+ "\t" + each.getCharge() + "\n";
               totalAmount += each.getCharge();
           }
           //add footer lines
           result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-          result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+          result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
           return result;
       }
 	   
@@ -43,6 +43,16 @@ public class Customer {
 		  while (rentals.hasMoreElements()){
 			  Rental each = (Rental) rentals.nextElement();
 			  result += each.getCharge();
+		  }
+		  return result;
+	  }
+	  
+	  private int getTotalFrequentRenterPoints(){
+		  int result=0;
+		  Enumeration rentals = _rentals.elements();
+		  while (rentals.hasMoreElements()){
+			  Rental each = (Rental) rentals.nextElement();
+			  result += each.getFrequentRenterPoints();
 		  }
 		  return result;
 	  }
--- PIS2Refactoring/src/main/java/org/pis2refactoring/Rental.java
+++ PIS2Refactoring/src/main/java/org/pis2refactoring/Rental.java
@@ -34,7 +34,7 @@ public class Rental {
 	  return result;
     }
     
-    int frequentRenterPoints(){
+    int getFrequentRenterPoints(){
         // add bonus for a two day new release rental
         if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
         	return 2;
